d8da07361408ee8316b9933e49b187479c77c549
empty87WorK <empty87@yandex.ru>
2022-06-04 13:34:46 +0300

    work with Fragment use (https://progler.ru/blog/kak-realizovat-vkladki-viewpager-i-fragment-v-android-s-pomoschyu-kotlin)

---
+++ app/src/main/java/com/emptyslon/kode/PageFragment.kt
@@ -0,0 +1,40 @@
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+class PageFragment : Fragment() {
+  private var mPage = 0
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    if (arguments != null) {
+      mPage = requireArguments().getInt(ARG_PAGE)
+//
+    }
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val view: View = inflater.inflate(R.layout.fragment_page, container, false)
+    val textView = view as TextView
+    textView.text = "Fragment #$mPage"
+    return view
+  }
+
+  companion object {
+    const val ARG_PAGE = "ARG_PAGE"
+    fun newInstance(page: Int): PageFragment {
+      val args = Bundle()
+      args.putInt(ARG_PAGE, page)
+      val fragment = PageFragment()
+      fragment.setArguments(args)
+      return fragment
+    }
+  }
+}
