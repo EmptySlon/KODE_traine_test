16db917bd4c57be37e1c3b698530965063df4680
empty87WorK <empty87@yandex.ru>
2022-06-30 10:12:10 +0300

    fix bag in inputSearch

--- app/src/main/java/com/emptyslon/kode/ListUserFragment.kt
+++ app/src/main/java/com/emptyslon/kode/ListUserFragment.kt
@@ -1,12 +1,14 @@
 package com.emptyslon.kode
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
@@ -19,17 +21,22 @@ import com.emptyslon.kode.dataBase.EmployeesData
 import com.emptyslon.kode.dataBase.EmployeesDataBase
 import com.emptyslon.kode.databinding.FragmentListUserBinding
 import com.emptyslon.kode.retrofit.RetrofitClient
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_list_user.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+const val DRAWABLE_RIGHT = 2
+
 class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
     lateinit var binding: FragmentListUserBinding
     private lateinit var options: Options
     var inputEmployees = listOf<Employee>()
     lateinit var filteredEmployees: List<Employee>
+    lateinit var typeSorted: String
+    lateinit var valueTad: String
 
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: AdapterEmploees
@@ -41,7 +48,7 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
     }
 
 
-    @SuppressLint("NotifyDataSetChanged")
+    @SuppressLint("NotifyDataSetChanged", "ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,16 +56,23 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
         binding = FragmentListUserBinding.inflate(inflater, container, false)
         AddTabInTabLayout()
 
+
+
+        typeSorted = getString(R.string.sorted_alphabet)
+        valueTad = Common.listDepartment.first()
+
+
         recyclerView = binding.recycleListUser
         recyclerView.layoutManager = LinearLayoutManager(activity)
         adapter = AdapterEmploees(inputEmployees)
         recyclerView.adapter = adapter
 
+
         getData()
 
         binding.tabCategory.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                val valueTad = tab!!.text.toString()
+                valueTad = tab!!.text.toString()
                 filteredEmployees = inputEmployees.filterFromDepartment(valueTad)
                 adapter.listEmployees = filteredEmployees
                 adapter.notifyDataSetChanged()
@@ -69,6 +83,9 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
         })
 
         binding.swipeRefresh.setOnRefreshListener {
+            binding.inputSearch
+                .compoundDrawables[DRAWABLE_RIGHT]
+                .setTint(requireActivity().getColor(R.color.grey2))
             requireActivity().supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.fragmentContainer, ListUserFragment())
@@ -91,8 +108,24 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
         })
 
+        binding.inputSearch.setOnTouchListener(View.OnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= inputSearch.right - inputSearch.compoundDrawables[DRAWABLE_RIGHT].bounds.width()) {
+                    showAlertOfSorted(inputSearch.compoundDrawables[DRAWABLE_RIGHT])
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+
 
         return binding.root
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 
     private fun getData() {
@@ -107,8 +140,8 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
                 listEmployees.map { it.avatarUrl = Common.listUrl.random() }
 
                 EmployeesDataBase.listEmployees = listEmployees.toMutableList()
-                inputEmployees =
-                    listEmployees.sortedByType(binding.root.context.getString(R.string.sorted_alphabet))
+                inputEmployees = listEmployees.sortedByType(typeSorted)
+                filteredEmployees = inputEmployees
 
                 adapter.listEmployees = inputEmployees
                 adapter.notifyDataSetChanged()
@@ -134,6 +167,28 @@ class ListUserFragment() : Fragment(), EmployeesDataBaseFun {
             newTab.text = department
             binding.tabCategory.addTab(newTab)
         }
+    }
+
+    private fun showAlertOfSorted(drawable: Drawable) {
+        val listTypeSorters =
+            arrayOf(getString(R.string.sorted_alphabet), getString(R.string.sorted_birthday))
+        val checkedItem = listTypeSorters.indexOf(typeSorted)
+
+        MaterialAlertDialogBuilder(requireActivity())
+            .setTitle(getString(R.string.sorting))
+            .setSingleChoiceItems(listTypeSorters, checkedItem) { dialogInterface, i ->
+                typeSorted = listTypeSorters[i]
+                inputEmployees = inputEmployees.sortedByType(typeSorted)
+                filteredEmployees = inputEmployees.filterFromDepartment(valueTad)
+                adapter.listEmployees = filteredEmployees
+                adapter.notifyDataSetChanged()
+                if (typeSorted == getString(R.string.sorted_birthday)) {
+                    drawable.setTint(requireActivity().getColor(R.color.purple_700))
+                } else drawable.setTint(requireActivity().getColor(R.color.grey2))
+                dialogInterface.dismiss()
+            }
+            .show()
+
     }
 
 
